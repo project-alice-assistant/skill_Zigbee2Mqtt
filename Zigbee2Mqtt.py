7f7bd46c90cedc018e2b0e64497699f5fab02dff
Psycho <laurentchervet@bluewin.ch>
2020-06-27 16:50:54 +0200

    Updating this until now useless skill :-)

--- Zigbee2Mqtt.py
+++ Zigbee2Mqtt.py
@@ -1,42 +1,112 @@
-import subprocess
+from typing import Optional
 
 from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import MqttHandler
 
 
-class Zigbee2Mqtt(AliceSkill):
+class Zigbee2Mqtt(AliceSkill): #NOSONAR
 	"""
 	Author: Psychokiller1888
 	Description: Have your zigbee devices communicate with alice directly over mqtt
 	"""
 
+	TOPIC_QUERY_DEVICE_LIST = 'zigbee2mqtt/bridge/config/devices'
+	TOPIC_PERMIT_JOIN = 'zigbee2mqtt/bridge/config/permit_join'
+	TOPIC_REMOVE_DEVICE = 'zigbee2mqtt/bridge/config/remove'
+	TOPIC_RENAME_DEVICE = 'zigbee2mqtt/bridge/config/rename'
+
+
 	def __init__(self):
 		self._online = False
+		self._devices = dict()
 		super().__init__()
 
 
 	@MqttHandler('zigbee2mqtt/bridge/state')
 	def bridgeStateReport(self, session: DialogSession):
-		if 'online' in session.payload:
+		if session.payload['state'] == b'online':
 			self._online = True
-			self.logInfo('Now online')
+			self.logInfo('Zigbee server online')
 		else:
 			self._online = False
-			self.logInfo('Now offline')
+			self.logInfo('Zigbee server offline')
 
 
-	@MqttHandler('zigbee2mqtt/#')
+	@MqttHandler('zigbee2mqtt/bridge/log')
 	def handleMessage(self, session: DialogSession):
-		print(session.payload)
+		logType = session.payload.get('type', None)
+		if not logType:
+			return
+
+		if logType == 'devices':
+			for device in session.payload['message']:
+				if device['type'] != 'EndDevice':
+					continue
+
+				self._devices[device['friendly_name']] = device
+		elif logType == 'device_removed':
+			self._devices.pop(session.payload['message'])
+		elif logType == 'device_renamed':
+			device = self._devices.pop(session.payload['from'], None)
+
+			if not device:
+				return
+
+			device['friendlyName'] = session.payload['to']
+			self._devices[session.payload['to']] = device
+
+
+	def getDevice(self, friendlyName: str) -> Optional[dict]:
+		return self._devices.get(friendlyName, None)
+
+
+	def renameDevice(self, friendlyName: str, newName: str) -> bool:
+		if ' ' in newName:
+			newName = newName.replace(' ', '_')
+
+		if newName in self._devices:
+			return False
+
+		self.publish(
+			topic=self.TOPIC_RENAME_DEVICE,
+			payload={
+				'old': friendlyName,
+				'new': newName
+			}
+		)
+
+		return True
+
+
+	def removeDevice(self, friendlyName: str):
+		self.publish(
+			topic=self.TOPIC_REMOVE_DEVICE,
+			stringPayload=friendlyName
+		)
+
+
+	def allowNewDeviceJoining(self):
+		self.publish(
+			topic=self.TOPIC_PERMIT_JOIN,
+			stringPayload='true'
+		)
+
+
+	def blockNewDeviceJoining(self):
+		self.publish(
+			topic=self.TOPIC_PERMIT_JOIN,
+			stringPayload='false'
+		)
 
 
 	def onStart(self):
 		super().onStart()
-		subprocess.run(['sudo', 'systemctl', 'start', 'zigbee2mqtt'])
-		self.MqttManager.publish(topic='zigbee2mqtt/bridge/config/permit_join', payload='false')
+		self.Commons.runRootSystemCommand(['systemctl', 'start', 'zigbee2mqtt'])
+		self.blockNewDeviceJoining()
+		self.publish(topic=self.TOPIC_QUERY_DEVICE_LIST)
 
 
 	def onStop(self):
 		super().onStop()
-		subprocess.run(['sudo', 'systemctl', 'stop', 'zigbee2mqtt'])
+		self.Commons.runRootSystemCommand(['systemctl', 'stop', 'zigbee2mqtt'])
